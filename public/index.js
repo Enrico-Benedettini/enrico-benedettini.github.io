ec17209ba10f0c081add37ba7e0972147e2607ee
Enrico-Benedettini <enrico.benedettini@gmail.com>
2024-07-15 18:11:53 +0200

    added iterative dropdown show/hide

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 // import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
-let dropdownContent = 
+let dropdownContents = {
+  "about":
 `
 <p>
-    As of une 2024, I am actively looking for a job after completing my studies at <strong>USI</strong> in Lugano, Switzerland.
+    As of June 2024, I am actively looking for a job after completing my studies at <strong>USI</strong> in Lugano, Switzerland.
     Currently I'm located in Lugano, after spending an unhappy semester at <strong>EPFL</strong> in Lausanne,
     and another semester at <strong>ETH</strong> in Zürich, which both are institutions globally renowned for their academic excellence.
     
@@ -47,7 +48,146 @@ let dropdownContent =
     a proficient project manager, contributing
     to the success of ambitious and impactful initiatives.
 </p>
+`,
+"soobr":
 `
+                        <p class="description-left">
+                            Full-Stack Developing for a cleaning effeciency focused company, providing an algorithm to efficiently clean the areas.
+                            Role as both backend and frontend developer.
+                            <br>
+                            Backend developing in Java with Spring 4 Frontend developing with Typescript with React.js <br>
+                            - <strong>Agile Software Development</strong>: Contributed to several product features as member of the development team using the Scrum methodology.
+                            <br>
+                            - <strong>Prototyping Data Solutions</strong>: Successfully implemented a prototype using the Google Cloud Platform (GCP) for data masking, anonymization, and database migration processes.
+                            <br>
+                            - <strong>Customer-Centric Support</strong>: Worked with the operations team to resolve bugs and handle urgent customer enquiries using the Kanban methodology.
+                            <br>
+                            - <strong>Advocating for Code Quality</strong>: Took the initiative to push code quality improvements within the team, including advocating for and introducing tools like Sonarlint plugin.
+                            <br>
+                        </p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1607681.114042065!2d7.0715817411199104!3d47.009588561283415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478e378ec0f45157%3A0xbfbb3ae16928a343!2sSoobr%20AG!5e1!3m2!1sit!2sch!4v1696493555021!5m2!1sit!2sch"
+                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                       
+`,
+"dina":
+`
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1359126.0985159317!2d11.015898114915313!3d44.46975356168108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132cb41ae0cb67a9%3A0x3b916d79c7ee9ae!2sLunardina%20la%20Pizza!5e1!3m2!1sit!2sit!4v1716219704816!5m2!1sit!2sit"
+                         width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <p class="description-right">
+                            Experience as a Waiter/Runner in Cervia, serving various customers, including lots of tourists.
+                            This work experience was undertaken to support my studies financially.
+                            Among the main strengths acquired doing this, I affirm the need for:
+                            <br>
+                            - <strong>Customer Service.</strong> Providing a welcoming and attentive service to enhance the dining experience.
+                            <br>
+                            - <strong>Multitasking.</strong> Handling multiple tables and tasks simultaneously in a fast-paced environment.
+                            <br>
+                            - <strong>Problem-Solving.</strong> Addressing and resolving any issues or complaints efficiently and courteously.
+                            <br>
+                            - <strong>Teamwork.</strong> Coordinating with kitchen staff and other waiters to ensure smooth service.
+                        </p>
+`,
+"glovo":
+`
+<p class="description-left">
+                            Labour work as Courier for Glovo, shipping deliveries from various providers to customers by car.
+                            <br>
+                            Work experience done to support my studies in Switzerland and allow me a more flexible schedule time for me in home town.
+                            Among the main strengths acquired doing this, I affirm the need for:
+                            <br> 
+                             - <strong>Precision and timing.</strong> Customers will get angry if the food is delivered late or cold reaches them already cold
+                            <br> 
+                             - <strong>Attention.</strong> Driving requires constant attention to be combined with customer service for clients waiting
+                            <br> 
+                             - <strong>Order.</strong> The delivery needs to be shipped in its completeness avoiding any loss on the way there
+                            <br> 
+                             - <strong>Communication.</strong> It's important to be clear and be able to communicate both with the provider and the customers.  
+                        </p>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1389460.706601911!2d11.099098989763673!3d44.41857278581738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132ca4d41ed493c1%3A0xbee0ab1075577a47!2sCesena%20FC!5e1!3m2!1sit!2sit!4v1716219639286!5m2!1sit!2sit"
+                         width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+`,
+"usi":
+`
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1810371.4733713416!2d7.232448167666164!3d46.103665085869814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4784327580543803%3A0xd99ed60a910d0f08!2sBiblioteca%20universitaria%20Lugano!5e1!3m2!1sit!2sit!4v1716219593920!5m2!1sit!2sit"
+                         width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <p class="description-right">
+                            Student-Assistant worker: Front-desk service for customers, rearrangement and sorting of the books.
+                            This job helped support my studies.
+                            Among the main strengths acquired doing this, I affirm the need for:
+                            <br>
+                            - <strong>Customer Service.</strong> Assisting customers efficiently at the front desk.
+                            <br>
+                            - <strong>Organizational Skills.</strong> Rearranging and sorting books systematically.
+                            <br>
+                            - <strong>Attention to Detail.</strong> Ensuring accurate classification and placement of books.
+                        
+                        </p>
+`,
+"martorano5":
+`
+<p class="description-left">
+                            A month of heavy manual work in a sperimental farm.
+                            Involved in various activities, ranging from picking vegetables to setting up the fields according to the necessary conditions for the specific type of vegetable.
+                            This summer job was useful to support my studies in Switzerland.
+                            Among the main strengths acquired doing this, I affirm the need for:
+                            <br>
+                            - <strong>Physical Stamina.</strong> Engaging in labor-intensive tasks for extended periods.
+                            <br>
+                            - <strong>Adaptability.</strong> Handling various tasks and adjusting to different farming and whether conditions.
+                            <br>
+                            - <strong>Attention to Detail.</strong> Ensuring the correct conditions for each type of vegetable.
+                            <br>
+                            - <strong>Teamwork.</strong> Collaborating with other workers to maintain efficient farm operations.
+                        </p>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d888641.1816489343!2d11.315423947461161!3d44.03913941631762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132cbac600a944fb%3A0x2fc029ddf2a9df78!2sAzienda%20Martorano%205%20Centrale%20Sperimentazioni%20e%20Servizi%20Agro-ambientali%20Soc.%20Cop.%20A.r.l.!5e1!3m2!1sit!2sit!4v1689608713678!5m2!1sit!2sit"
+                         width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+`,
+"jmol":
+`
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3060629.4009721866!2d-6.497379114474647!3d37.92047951442713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126e97fdf41d33%3A0x42fdf52c80acb1f6!2sJmol%20Inform%C3%A1tica%20SLU!5e1!3m2!1sit!2sit!4v1689606251530!5m2!1sit!2sit"
+                         width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                         <p class="description-right">
+                            Experience part of Erasmus+, a program offering work, room, and board, for which I won the tender in my high school.
+                            <br>
+                            There, I could take care of troubleshooting for various clients, fixing the network, and providing customer service. Also developed an archive management software to handle receipts from every customer.
+                            <br>
+                            The job presented challenges such as adapting to a different language and working hours, as Spain has a unique schedule due to high temperatures.
+                            <br>
+                            Among the main strengths acquired doing this, I affirm the need for:
+                            <br>
+                            - <strong>Technical Skills.</strong> Troubleshooting and fixing network issues for clients.
+                            <br>
+                            - <strong>Software Development.</strong> Developing an archive management software.
+                            <br>
+                            - <strong>Adaptability.</strong> Overcoming language barriers and adjusting to different work hours.
+                            <br>
+                            - <strong>Customer Service.</strong> Providing efficient and effective service to clients.
+                        </p>
+`,
+"grupposistema":
+`
+<p class="description-left">
+                            This project was part of the 'Alternanza scuola-lavoro' program, developed as a 3rd-year high school student.
+                            <br>
+                            Created a website for a library. Initially, we modeled the backend structure, applying <strong>Object-Oriented Programming (OOP)</strong>
+                            principles to structure user interaction with the system using <strong>C#</strong> and <strong>.NET 4.0</strong>. Later, the site was developed using <strong>HTML</strong>, <strong>CSS</strong>, and <strong>jQuery</strong> 
+                            for static content, and then transitioned to WordPress for dynamic content management.
+                            <br>
+                            Among the main strengths acquired doing this, I affirm the need for:
+                            <br>
+                            - <strong>Web Development Skills.</strong> Building and designing websites using various technologies.
+                            <br>
+                            - <strong>Backend Development.</strong> Modeling backend structures and applying OOP principles.
+                            <br>
+                            - <strong>Adaptability.</strong> Transitioning from static to dynamic web development.
+                            <br>
+                            - <strong>Project Management.</strong> Completing a significant project within a set timeframe.
+                            <br>
+                            - <strong>Problem-Solving.</strong> Addressing and overcoming technical challenges during development.
+                        </p>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1636746.6836726335!2d11.611325487676194!3d44.00557780125613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132ca4cda208d003%3A0x11d98757a965e8b!2sIIS%20Pascal%20Comandini%20-%20sede%20ITT%20%22Blaise%20Pascal%22!5e1!3m2!1sit!2sit!4v1689606503169!5m2!1sit!2sit"
+                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+`
+}
 
 
 // function to hide the navbar after 5 seconds of inactivity
@@ -94,22 +234,26 @@ function setNavLinksListeners() {
 }
 
 function setDropdownListeners() {
-  let btn = document.querySelector('.btn-dropdown');
-  btn.addEventListener('click', () => {
-    // content.classList.add('hidden');
-    let aboutDiv = document.querySelector('#about > .container')
-    if(btn.querySelector('i').classList.contains('fa-ellipsis')) {
-      btn.innerHTML = `<i class="fa-solid fa-caret-up"></i>`;
-      let content = document.createElement('div');
-      content.innerHTML = dropdownContent;
-      content.classList.add('dropdown');
-      aboutDiv.appendChild(content);
-    } else {
-      btn.innerHTML = `<i class="fa-solid fa-ellipsis"></i>`;
-      aboutDiv.removeChild(aboutDiv.lastChild);
-    }
-    
-  });
+  let btns = document.querySelectorAll('.btn-dropdown');
+  let keys = Object.keys(dropdownContents);
+  for(let i=0; i<btns.length; i++) {
+    let key = keys[i];
+    btns[i].addEventListener('click', () => {
+      let div = document.querySelector(`.${key}`)
+      showOrHideContent(btns[i],div,dropdownContents[key],key);
+    });
+  }
+}
+
+function showOrHideContent(btn, parentDiv, dropdownContent, key) {
+  let content = parentDiv.querySelector('.dropdown');
+  if(btn.querySelector('i').classList.contains('fa-ellipsis')) {
+    btn.innerHTML = `<i class="fa-solid fa-caret-up"></i>`;
+    content.innerHTML = dropdownContent;
+  } else {
+    btn.innerHTML = `<i class="fa-solid fa-ellipsis"></i>`;
+    content.innerHTML = '';
+  }
 }
 
 function setNightModeListener() {
