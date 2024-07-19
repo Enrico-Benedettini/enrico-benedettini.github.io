fcd23ce94849a99a3c77720674bfabe549897e27
Enrico-Benedettini <enrico.benedettini@gmail.com>
2024-07-19 17:52:26 +0200

    added mobile view navbar

--- public/index.js
+++ public/index.js
@@ -189,43 +189,88 @@ let dropdownContents = {
 `
 }
 
+function createMobileButton() {
+  // Check if the button already exists
+  if (document.getElementById('navbar-toggle')) return;
+
+  // Create the button element
+  const toggleButton = document.createElement('button');
+  toggleButton.classList.add('navbar-toggle');
+  toggleButton.id = 'navbar-toggle';
+
+  // Create the span elements for the bars
+  for (let i = 0; i < 3; i++) {
+    const bar = document.createElement('span');
+    bar.classList.add('bar');
+    toggleButton.appendChild(bar);
+  }
+
+  // Add the button to the navbar header
+  const navbarHeader = document.querySelector('.navbar-header');
+  navbarHeader.appendChild(toggleButton);
+
+  // Add event listener to toggle the navbar links
+  toggleButton.addEventListener('click', function() {
+    const navbarLinks = document.getElementById('navbar-links');
+    navbarLinks.classList.toggle('drop');
+  });
+}
+
+function checkWindowSize() {
+  if (window.innerWidth <= 768) {
+    createMobileButton();
+  } else {
+    // Remove the button if it exists and the window is resized to a larger size
+    const toggleButton = document.getElementById('navbar-toggle');
+    if (toggleButton) {
+      toggleButton.remove();
+      const navbarLinks = document.getElementById('navbar-links');
+      navbarLinks.classList.remove('drop');
+    }
+  }
+}
 
 // function to hide the navbar after 5 seconds of inactivity
 function setNavbarHiding() {
-  let timeout;
-  let prevScrollPos = window.pageYOffset;
-  const navbar = document.querySelector('.navbar');
+  
+  if (window.innerWidth > 768) {
+    let timeout;
+    let prevScrollPos = window.pageYOffset;
+    const navbar = document.querySelector('.navbar');
 
-  if (prevScrollPos == 0) {
-    navbar.style.position = 'static'
-  } else {
-    navbar.style.position = 'fixed'
-  }
-  window.addEventListener('scroll', () => {
-    clearTimeout(timeout);
-    navbar.style.top = '0';
-    
-    const currentScrollPos = window.pageYOffset;
-    if (prevScrollPos < currentScrollPos && currentScrollPos != 0) {
-      navbar.style.top = '-100px'; // Adjust this value based on the navbar height
-    } else if (currentScrollPos == 0) {
+    if (prevScrollPos == 0) {
       navbar.style.position = 'static'
-      navbar.classList.remove('shadow')
     } else {
       navbar.style.position = 'fixed'
-      navbar.classList.add('shadow')
     }
-    
-    prevScrollPos = currentScrollPos;
-    
-    timeout = setTimeout(() => {
-      navbar.style.top = '-100px'; // Adjust this value based on the navbar height
-    }, 5000); // Set the timeout value (in milliseconds) before the navbar hides
+  }
+  window.addEventListener('scroll', () => {
+    if (window.innerWidth > 768) {
+      clearTimeout(timeout);
+      navbar.style.top = '0';
+      
+      const currentScrollPos = window.pageYOffset;
+      if (prevScrollPos < currentScrollPos && currentScrollPos != 0) {
+        navbar.style.top = '-100px'; // Adjust this value based on the navbar height
+      } else if (currentScrollPos == 0) {
+        navbar.style.position = 'static'
+        navbar.classList.remove('shadow')
+      } else {
+        navbar.style.position = 'fixed'
+        navbar.classList.add('shadow')
+      }
+      
+      prevScrollPos = currentScrollPos;
+      
+      timeout = setTimeout(() => {
+        navbar.style.top = '-100px'; // Adjust this value based on the navbar height
+      }, 5000); // Set the timeout value (in milliseconds) before the navbar hides
+    }
   });
 }
 
 function setNavLinksListeners() {
-  document.querySelectorAll(".navbar-content a").forEach(a=>{
+  document.querySelectorAll(".navbar-links a").forEach(a=>{
     a.addEventListener('click',(e)=>{
       document.querySelector('.active').classList.remove('active')
       a.classList.add('active');
@@ -286,7 +331,6 @@ function setNightModeListener() {
     switchToOppositeColor('--link-text')
     switchToOppositeColor('--hover-text')
     switchToOppositeColor('--text-home')
-    switchToOppositeColor('--text-skills')
     switchToOppositeColor('--border-line')
     switchToOppositeColor('--background-minigames')
     switchToOppositeColor('--text-minigames')
@@ -332,105 +376,28 @@ function switchToOppositeColor(variable) {
   root.style.setProperty(variable,firstOppositeColor)
 }
 
-async function createPdfFromString(content, title, author) {
-  const pdfDoc = await PDFDocument.create();
-  const page = pdfDoc.addPage([500, 200]); // Set the page size (width and height)
+function setToggleListener() {
+  const toggleButton = document.getElementById('navbar-toggle');
+  const navbarLinks = document.getElementById('navbar-links');
 
-  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-  const { width, height } = page.getSize();
-  const textSize = 12;
-  
-  const text = page.drawText(content, {
-    x: 50,
-    y: height - 50,
-    size: textSize,
-    font: font,
-    color: rgb(0, 0, 0),
-  });
-
-  // Set metadata (title and author) for the PDF
-  pdfDoc.setTitle(title || '');
-  pdfDoc.setAuthor(author || '');
-
-  const pdfBytes = await pdfDoc.save();
+  if (toggleButton) { 
+    toggleButton.addEventListener('click', function() {
+      navbarLinks.classList.toggle('drop');
+    });
+  }
 }
 
-function automatizedWordsReplacer(src,positionName,recipientName,companyName,companyAddress,companyCity,companyState,companyCountry,companyZipCode) {
-  // snippet of code found at https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
-  let today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0');
-  const yyyy = today.getFullYear();
-  today = dd + '/' + mm + '/' + yyyy;
 
-  console.log(today)
-  src = src.replace("[Date]", today)
-  src = src.replaceAll("[Recipient's Name]", recipientName);
-  src = src.replaceAll("[Position Name]", positionName);
-  src = src.replaceAll("[Company Name]", companyName);
-  src = src.replaceAll("[Company Address]", companyAddress);
-  src = src.replace("[City, State, Country, ZIP Code]", `${companyCity}, ${companyState}, ${companyCountry}, ${companyZipCode}`)
-  return src
-}
-
-function insertCompanyData() {
-  // First, we take all the data from the input tags
-  let companyData = document.querySelector('.company-data')
-  let positionName = companyData.querySelector('input[name="position-name"]').value
-  let recipientName = companyData.querySelector('input[name="recipient-name"]').value
-  let companyName = companyData.querySelector('input[name="company-name"]').value
-  let companyAddress = companyData.querySelector('input[name="company-address"]').value
-  let companyCity = companyData.querySelector('input[name="company-city"]').value
-  let companyState = companyData.querySelector('input[name="company-state"]').value
-  let companyCountry = companyData.querySelector('input[name="company-country"]').value
-  let companyZipCode = companyData.querySelector('input[name="company-zip-code"]').value
-  let src = document.querySelector('textarea[name="source"]').value
-
-  let result = automatizedWordsReplacer(src,positionName,recipientName,companyName,companyAddress,companyCity,companyState,companyCountry,companyZipCode)
-
-  document.querySelector('textarea[name="result"]').value = result
-
-  createPdfFromString(result,'CoverLetter','Enrico Benedettini')
-}
-
-function setInsertButtonListener() {
-  document.querySelector(`input[name="insert"]`).addEventListener('click',()=>{
-    insertCompanyData()
-  })
-}
-
+checkWindowSize()
 setNavbarHiding()
 setNavLinksListeners()
 setNightModeListener()
 setDropdownListeners()
-// setInsertButtonListener()
+setToggleListener()
 
-let coverLetter = `
-Enrico Benedettini
-Via Zurigo 3
-Lugano, Switzerland, 6900
-enrico.benedettini@gmail.com
-+39 3453329757
-28/07/2023
 
-[Recipient's Name]
-[Company Name]
-[Company Address]
-[City, State, ZIP Code]
+window.addEventListener('resize', checkWindowSize);
 
-Dear [Recipient's Name],
-
-I am writing to express my strong interest in the [Position Name] role at [Company Name]. As a final year student at Università della Svizzera italiana, with a passion for backend development and algorithms optimization, I am excited about the opportunity to contribute my skills and experiences to a dynamic software company like [Company Name].
-
-Throughout my academic journey at ETH and EPFL, I have actively sought to apply my knowledge in real-world settings. At USI, I led a team of 5 developers in implementing authentication with OAuth2 APIs and achieving 97% code coverage with top-quality ratings. I also co-led a team of 14 in developing a Web-App, where I optimized backend performance using SpringBoot and enhanced frontend animations with Vue.js.
-
-My proficiency extends to various technologies, including Java, C#, and frameworks like Vue.js. Additionally, I am currently exploring AI and machine learning, taking an introduction to Machine Learning this semester, which I believe will further complement my skill set.
-
-I find immense joy in collaborating with teams, understanding the importance of the human side in driving successful projects. As I aspire to grow and learn continuously, I am particularly drawn to [Company Name]'s commitment to innovation and technological advancement.
-
-I would welcome the opportunity to discuss how my experiences align with [Company Name]'s vision and how I can contribute to your continued success. Thank you for considering my application. I eagerly look forward to the possibility of joining your talented team.
-
-Sincerely,
-
-[Your Name]
-`
+document.addEventListener('DOMContentLoaded', function() {
+  setToggleListener();
+});
